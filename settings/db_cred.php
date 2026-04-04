cba814f74c014c5d14f8a841b06e37fa69307d6d
Jemima Antwi <jemimaantwi47@gmail.com>
2026-04-04 05:46:51 +0000

    Correct MySQL hostname for InfinityFree

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,7 +1,7 @@
 <?php
 //Database credentials
 
-define("SERVER", "sql111.epizy.com");   // Note: The host usually depends on the specific InfinityFree server, but I will prompt the user if this fails.
+define("SERVER", "sql211.infinityfree.com");   
 define("USERNAME", "if0_41570830");      
 define('PASSWD', 'lR4nGHZdIzeq');           
 define("DATABASE", "if0_41570830_preorda");       
